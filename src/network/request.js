be8e8e56b56a8615e4893d58ef39686a014d9eb9
chostan <56330392+chostan@users.noreply.github.com>
2021-10-11 15:15:16 +0800

    修改接口地址

--- src/network/request.js
+++ src/network/request.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+// 本地接口:http://127.0.0.1:8888/api/private/v1/'
+// https://www.liulongbin.top:8888/api/private/v1/
+// 这个接口会重置数据http://www.ysqorz.top:8888/api/private/v1/
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8888/api/private/v1/',
+    baseURL: 'http://www.ysqorz.top:8888/api/private/v1/',
     timeout: 5000
   })
 
